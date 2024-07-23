39d8cc50e46a325c372febbd08070d3481df78a2
Salman <salmanfaris98@gmail.com>
2024-07-23 10:06:31 +0100

    add: Dockerfile and shell script to run docker

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.11-slim
+
+# Expose the port you want your app to run on
+EXPOSE 8080
+
+# Set WORKDIR
+WORKDIR /app
+
+# Upgrade pip and install requirements
+COPY requirements.txt requirements.txt
+RUN pip install -U pip
+RUN pip install -r requirements.txt
+
+# Copy app files into root
+COPY /app .
+
+# Run
+ENTRYPOINT ["streamlit", "run", "app.py", "--server.port=8080", "--server.address=0.0.0.0"]
---
+++ dockerize.sh
@@ -0,0 +1,16 @@
+IMAGE_NAME="streamlit-mvp"
+
+# PLAIN ENGLISH: 
+#   Find the local `Dockerfile` in the current directory `.` and build an image based 
+#   on the instructions in there. The `-t` flag tells Docker to give the image a name
+#   and here we specify the name to be `$IMAGE_NAME`
+docker build -t $IMAGE_NAME .
+
+# PLAIN ENGLISH:
+#   Run the container using the built image `$IMAGE_NAME`, exposing port 8080 for the
+#   host port and 8080 for the container port (both is a choice). We specify the ports 
+#   using the `-p` flag. The `--rm` flag tells Docker to remove the container (not the 
+#   image!) after it stops running.
+HOST_PORT=8080
+CONTAINER_PORT=8080
+docker run --rm -p $HOST_PORT:$CONTAINER_PORT $IMAGE_NAME
